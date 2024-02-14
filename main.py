d882f151f020142dc6e05c976b92ff37db96d4e2
RedAlf <francismirezDPL@gmail.com>
2024-02-14 14:55:36 -0500

    secuential tree builded

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ import secuential_tree.rules as rules
 if __name__ == "__main__":
     test_database = "data/obessity.csv"
     data = pd.read_csv(test_database)
-    #!REMEMBER TO COMMENT THESE SENTENCES TO TEST ANOTHER DATA
-    data['obese'] = (data.Index >= 4).astype('int').astype('str')
-    data.drop('Index', axis = 1, inplace = True)
-    print(rules.gini_impurity(data.Gender))
-    print(rules.entropy(data.Gender))
-    print(rules.information_gain(data['obese'], data['Gender'] == 'Male'))
+    print(data.head())
+    max_depth = 5
+    min_samples_split = 20
+    min_information_gain  = 1e-5
+    
+    
     #*Checking secuential tree
+    start = t.time()
+    decisiones = rules.train_tree(data,'Index',True, max_depth,min_samples_split,min_information_gain)
+    end = t.time()
+    #print(decisiones)
+    print(f"Tiempo transcurrido {end-start}")
     #*Checking parallel tree
     #build_tree(Tree(dataset))
     #build_tree(ParallelTree(dataset))
